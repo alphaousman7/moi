76edb11fc3f9237410d8899338581408083e98e0
Alpha ousmane diallo <121781323+alphaousman7@users.noreply.github.com>
2023-03-18 14:54:29 +0100

    Video.jsx
    change de positionnement

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactPlayer from 'react-player'
 export default function Video() {
   return (
-    <div name='vide'><h1>Video</h1>
+    <div name='vide'><h1>Video de verification </h1>
     <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
     </div>
   )
