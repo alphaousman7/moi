72769e1614bdfcc68d33343af2eee84235f143a8
Alpha ousmane diallo <alphadiaous@gmail.com>
2023-03-18 15:21:56 +0100

    changement de positionnement du video

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import ReactPlayer from 'react-player'
 export default function Video() {
   return (
-    <div name='vide'><h1>Video</h1>
+    <div name='vide' className='shadow-md hover:scale-105 duration-300 rounded-2xl mx-auto w-1/3 md:w-full'>
+      <h1>Video a ne pas modifier</h1>
+      <h1>Video a ne pas modifier</h1>
     <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
     </div>
   )
