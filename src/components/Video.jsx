49644c5d5c92b42a448d2cb0a66908a3f78c1b68
Alpha ousmane diallo <alphadiaous@gmail.com>
2023-03-18 15:34:41 +0100

    changement de position

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,7 +3,7 @@ import ReactPlayer from 'react-player'
 export default function Video() {
   return (
     <div name='vide' className='shadow-md hover:scale-105 duration-300 rounded-2xl mx-auto w-1/3 md:w-full'>
-      <h1>Video a ne pas modifier</h1>
+      <h1>Video a ne pas modifier du tous mon chere</h1>
       <h1>Video a ne pas modifier</h1>
     <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U' />
     </div>
